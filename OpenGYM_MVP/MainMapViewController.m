6ad1a7e66f53ec5e0485b2fd14f926cdd2e198eb
Harrison Ferrone <hferrone@gmail.com>
2015-01-14 19:26:35 -0600

    Map pins filter by sport working and tested

--- OpenGYM_MVP/MainMapViewController.m
+++ OpenGYM_MVP/MainMapViewController.m
@@ -14,12 +14,22 @@
 @interface MainMapViewController ()
 
 @property (weak, nonatomic) IBOutlet UIView *sportSelectionPopoverView;
+
 @property (weak, nonatomic) IBOutlet UIButton *basketballSelectedButton;
+
 @property (weak, nonatomic) NSString *sportSelected;
-@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (weak, nonatomic) NSString *addressString;
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (weak, nonatomic) CLPlacemark *topResult;
 @property MKPlacemark *placemark;
+
+@property NSArray *allEventsArray;
+@property NSArray *basketballEventArray;
+@property NSMutableArray *soccerEventArray;
+@property NSMutableArray *baseballEventArray;
+@property NSMutableArray *tennisEventArray;
+@property NSMutableArray *footballEventArray;
 
 @end
 
@@ -29,35 +39,6 @@
 {
     [super viewDidLoad];
     
-//    PFQuery *query = [PFQuery queryWithClassName:@"Event"];
-//    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
-//    {
-//        for(PFObject *object in objects)
-//        {
-//            self.addressString = object[@"address"];
-//            NSString *new = [self.addressString stringByAppendingString:@" Chicago, IL"];
-//            NSLog(@"%@", self.addressString);
-//            
-//            NSString *location = new;
-//            CLGeocoder *geocoder = [[CLGeocoder alloc] init];
-//            [geocoder geocodeAddressString:location
-//                         completionHandler:^(NSArray* placemarks, NSError* error){
-//                             if (placemarks && placemarks.count > 0) {
-//                                 CLPlacemark *topResult = [placemarks objectAtIndex:0];
-//                                 MKPlacemark *placemark = [[MKPlacemark alloc] initWithPlacemark:topResult];
-//                                 
-//                                 MKCoordinateRegion region = self.mapView.region;
-//                                 region.center = placemark.region.center;
-//                                 region.span.longitudeDelta /= 8.0;
-//                                 region.span.latitudeDelta /= 8.0;
-//                                 
-//                                 [self.mapView setRegion:region animated:YES];
-//                                 [self.mapView addAnnotation:placemark];
-//                             }
-//                         }
-//             ];
-//        }
-//    }];
 }
 
 - (IBAction)overlayOnButtonTapped:(UIButton *)sender
@@ -69,6 +50,9 @@
 
 -(void)queryBySportSelected
 {
+    NSArray *allPoints = self.mapView.annotations;
+    [self.mapView removeAnnotations:allPoints];
+    
     NSLog(@"%@", self.sportSelected);
     [UIView animateWithDuration:0.3 animations:^{
         self.sportSelectionPopoverView.frame = CGRectMake(600, 600, 5, 5);
@@ -96,10 +80,29 @@
                                   
                                   if([self.sportSelected isEqualToString:@"Basketball"])
                                   {
-                                      
+                                      self.basketballEventArray = [NSMutableArray arrayWithObject:annotation];
+                                      [self.mapView addAnnotations:self.basketballEventArray];
                                   }
-                                  
-                                  [self.mapView addAnnotation:annotation];
+                                  else if([self.sportSelected isEqualToString:@"Soccer"])
+                                  {
+                                      self.soccerEventArray = [NSMutableArray arrayWithObject:annotation];
+                                      [self.mapView addAnnotations:self.soccerEventArray];
+                                  }
+                                  else if([self.sportSelected isEqualToString:@"Baseball"])
+                                  {
+                                      self.baseballEventArray = [NSMutableArray arrayWithObject:annotation];
+                                      [self.mapView addAnnotations:self.baseballEventArray];
+                                  }
+                                  else if([self.sportSelected isEqualToString:@"Tennis"])
+                                  {
+                                      self.tennisEventArray = [NSMutableArray arrayWithObject:annotation];
+                                      [self.mapView addAnnotations:self.tennisEventArray];
+                                  }
+                                  else if([self.sportSelected isEqualToString:@"Football"])
+                                  {
+                                      self.footballEventArray = [NSMutableArray arrayWithObject:annotation];
+                                      [self.mapView addAnnotations:self.footballEventArray];
+                                  }
                               }
                           }
               ];
