386fdc183b9caadf0299534fa5f1cc55eb4d8325
Harrison Ferrone <hferrone@gmail.com>
2015-01-16 11:14:54 -0600

    Reveal Menu layout finished, code not in yet

---
+++ OpenGYM_MVP/SlideOutViewController.h
@@ -0,0 +1,13 @@
+//
+//  SlideOutViewController.h
+//  OpenGYM_MVP
+//
+//  Created by Harrison Ferrone on 1/16/15.
+//  Copyright (c) 2015 Harrison Ferrone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SlideOutViewController : UIViewController
+
+@end
