3e28d15a35f504916d9ec038e7eb2baff224bea6
Harrison Ferrone <hferrone@gmail.com>
2015-01-28 11:37:53 -0600

    Revert

---
+++ ShareAppExtension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  ShareAppExtension
+//
+//  Created by Harrison Ferrone on 1/28/15.
+//  Copyright (c) 2015 Harrison Ferrone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
