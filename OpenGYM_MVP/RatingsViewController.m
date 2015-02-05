f101c8cca257778b804290c640e432c233101565
Harrison Ferrone <hferrone@gmail.com>
2015-02-05 11:16:35 -0600

    Graph plugin problem resolved

--- OpenGYM_MVP/RatingsViewController.m
+++ OpenGYM_MVP/RatingsViewController.m
@@ -8,10 +8,13 @@
 
 #import "RatingsViewController.h"
 #import "SWRevealViewController.h"
+#import "CorePlot-CocoaTouch.h"
 
 @interface RatingsViewController ()
 
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
+@property (nonatomic, strong) CPTGraphHostingView *hostView;
+@property (weak, nonatomic) IBOutlet UIView *ratingsView;
 
 @end
 
@@ -20,6 +23,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self initPlot];
     
     SWRevealViewController *revealViewController = self.revealViewController;
     if ( revealViewController )
@@ -34,6 +38,59 @@
 -(BOOL)prefersStatusBarHidden
 {
     return true;
+}
+
+#pragma mark - Chart behavior
+-(void)initPlot
+{
+    [self configureHost];
+    [self configureGraph];
+    [self configurePlots];
+    [self configureAxes];
+}
+
+-(void)configureHost
+{
+    self.hostView = [(CPTGraphHostingView *) [CPTGraphHostingView alloc] initWithFrame:self.view.bounds];
+    self.hostView.allowPinchScaling = YES;
+    [self.view addSubview:self.hostView];
+}
+
+-(void)configureGraph
+{
+    // 1 - Create the graph
+    CPTGraph *graph = [[CPTXYGraph alloc] initWithFrame:self.hostView.bounds];
+
+    // 2 - Set graph title
+    NSString *title = @"Player Rating";
+    graph.title = title;
+    
+    // 3 - Create and set text style
+    CPTMutableTextStyle *titleStyle = [CPTMutableTextStyle textStyle];
+    titleStyle.color = [CPTColor orangeColor];
+    titleStyle.fontName = @"Helvetica-Bold";
+    titleStyle.fontSize = 16.0f;
+    graph.titleTextStyle = titleStyle;
+    graph.titlePlotAreaFrameAnchor = CPTRectAnchorTop;
+    graph.titleDisplacement = CGPointMake(0.0f, 10.0f);
+    
+    // 4 - Set padding for plot area
+    [graph.plotAreaFrame setPaddingLeft:30.0f];
+    [graph.plotAreaFrame setPaddingBottom:30.0f];
+    
+    // 5 - Enable user interactions for plot space
+    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *) graph.defaultPlotSpace;
+    plotSpace.allowsUserInteraction = YES;
+}
+
+-(void)configurePlots
+{
+    
+}
+
+-(void)configureAxes
+{
+    
 }
 
 @end
