e1f53c529b271e4a5f267dfb0b231b54f2e0ea06
Harrison Ferrone <hferrone@gmail.com>
2015-02-05 12:17:58 -0600

    revert

--- OpenGYM_MVP/RatingsViewController.m
+++ OpenGYM_MVP/RatingsViewController.m
@@ -10,7 +10,7 @@
 #import "SWRevealViewController.h"
 #import "CorePlot-CocoaTouch.h"
 
-@interface RatingsViewController ()
+@interface RatingsViewController () <CPTPlotDataSource>
 
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
 @property (nonatomic, strong) CPTGraphHostingView *hostView;
@@ -40,6 +40,17 @@
     return true;
 }
 
+#pragma mark - CPTPlotDataSource methods
+-(NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plot
+{
+    return 0;
+}
+
+-(NSNumber *)numberForPlot:(CPTPlot *)plot field:(NSUInteger)fieldEnum recordIndex:(NSUInteger)index
+{
+    return [NSDecimalNumber zero];
+}
+
 #pragma mark - Chart behavior
 -(void)initPlot
 {
@@ -51,7 +62,7 @@
 
 -(void)configureHost
 {
-    self.hostView = [(CPTGraphHostingView *) [CPTGraphHostingView alloc] initWithFrame:self.view.bounds];
+    self.hostView = [(CPTGraphHostingView *) [CPTGraphHostingView alloc] initWithFrame:self.ratingsView.frame];
     self.hostView.allowPinchScaling = YES;
     [self.view addSubview:self.hostView];
 }
@@ -60,7 +71,9 @@
 {
     // 1 - Create the graph
     CPTGraph *graph = [[CPTXYGraph alloc] initWithFrame:self.hostView.bounds];
-
+    [graph applyTheme:[CPTTheme themeNamed:kCPTDarkGradientTheme]];
+    self.hostView.hostedGraph = graph;
+    
     // 2 - Set graph title
     NSString *title = @"Player Rating";
     graph.title = title;
@@ -85,7 +98,38 @@
 
 -(void)configurePlots
 {
+    // 1 - Get graph and plot space
+    CPTGraph *graph = self.hostView.hostedGraph;
+    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *) graph.defaultPlotSpace;
     
+    // 2 - Create the three plots
+    CPTScatterPlot *aaplPlot = [[CPTScatterPlot alloc] init];
+    aaplPlot.dataSource = self;
+    //aaplPlot.identifier = CPDTickerSymbolAAPL;
+    CPTColor *aaplColor = [CPTColor redColor];
+    [graph addPlot:aaplPlot toPlotSpace:plotSpace];
+    
+    // 3 - Set up plot space
+    [plotSpace scaleToFitPlots:[NSArray arrayWithObjects:aaplPlot, nil]];
+    CPTMutablePlotRange *xRange = [plotSpace.xRange mutableCopy];
+    [xRange expandRangeByFactor:CPTDecimalFromCGFloat(1.1f)];
+    plotSpace.xRange = xRange;
+    CPTMutablePlotRange *yRange = [plotSpace.yRange mutableCopy];
+    [yRange expandRangeByFactor:CPTDecimalFromCGFloat(1.2f)];
+    plotSpace.yRange = yRange;
+    
+    // 4 - Create styles and symbols
+    CPTMutableLineStyle *aaplLineStyle = [aaplPlot.dataLineStyle mutableCopy];
+    aaplLineStyle.lineWidth = 2.5;
+    aaplLineStyle.lineColor = aaplColor;
+    aaplPlot.dataLineStyle = aaplLineStyle;
+    CPTMutableLineStyle *aaplSymbolLineStyle = [CPTMutableLineStyle lineStyle];
+    aaplSymbolLineStyle.lineColor = aaplColor;
+    CPTPlotSymbol *aaplSymbol = [CPTPlotSymbol ellipsePlotSymbol];
+    aaplSymbol.fill = [CPTFill fillWithColor:aaplColor];
+    aaplSymbol.lineStyle = aaplSymbolLineStyle;
+    aaplSymbol.size = CGSizeMake(6.0f, 6.0f);
+    aaplPlot.plotSymbol = aaplSymbol;
 }
 
 -(void)configureAxes
