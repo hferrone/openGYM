c1f065256c87ff167af1d2fd931edb3d2cbc990c
Harrison Ferrone <hferrone@gmail.com>
2015-02-05 12:28:50 -0600

    Revert 27f63f9..4ca73d2
    This rolls back to commit 27f63f98ef9cc373f44647b4c576f7453b5d2424.

--- OpenGYM_MVP/RatingsViewController.m
+++ OpenGYM_MVP/RatingsViewController.m
@@ -8,13 +8,10 @@
 
 #import "RatingsViewController.h"
 #import "SWRevealViewController.h"
-#import "CorePlot-CocoaTouch.h"
 
-@interface RatingsViewController () <CPTPlotDataSource>
+@interface RatingsViewController ()
 
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebarButton;
-@property (nonatomic, strong) CPTGraphHostingView *hostView;
-@property (weak, nonatomic) IBOutlet UIView *ratingsView;
 
 @end
 
@@ -23,7 +20,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self initPlot];
     
     SWRevealViewController *revealViewController = self.revealViewController;
     if ( revealViewController )
@@ -38,103 +34,6 @@
 -(BOOL)prefersStatusBarHidden
 {
     return true;
-}
-
-#pragma mark - CPTPlotDataSource methods
--(NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plot
-{
-    return 0;
-}
-
--(NSNumber *)numberForPlot:(CPTPlot *)plot field:(NSUInteger)fieldEnum recordIndex:(NSUInteger)index
-{
-    return [NSDecimalNumber zero];
-}
-
-#pragma mark - Chart behavior
--(void)initPlot
-{
-    [self configureHost];
-    [self configureGraph];
-    [self configurePlots];
-    [self configureAxes];
-}
-
--(void)configureHost
-{
-    self.hostView = [(CPTGraphHostingView *) [CPTGraphHostingView alloc] initWithFrame:self.ratingsView.frame];
-    self.hostView.allowPinchScaling = YES;
-    [self.view addSubview:self.hostView];
-}
-
--(void)configureGraph
-{
-    // 1 - Create the graph
-    CPTGraph *graph = [[CPTXYGraph alloc] initWithFrame:self.hostView.bounds];
-    [graph applyTheme:[CPTTheme themeNamed:kCPTDarkGradientTheme]];
-    self.hostView.hostedGraph = graph;
-    
-    // 2 - Set graph title
-    NSString *title = @"Player Rating";
-    graph.title = title;
-    
-    // 3 - Create and set text style
-    CPTMutableTextStyle *titleStyle = [CPTMutableTextStyle textStyle];
-    titleStyle.color = [CPTColor orangeColor];
-    titleStyle.fontName = @"Helvetica-Bold";
-    titleStyle.fontSize = 16.0f;
-    graph.titleTextStyle = titleStyle;
-    graph.titlePlotAreaFrameAnchor = CPTRectAnchorTop;
-    graph.titleDisplacement = CGPointMake(0.0f, 10.0f);
-    
-    // 4 - Set padding for plot area
-    [graph.plotAreaFrame setPaddingLeft:30.0f];
-    [graph.plotAreaFrame setPaddingBottom:30.0f];
-    
-    // 5 - Enable user interactions for plot space
-    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *) graph.defaultPlotSpace;
-    plotSpace.allowsUserInteraction = YES;
-}
-
--(void)configurePlots
-{
-    // 1 - Get graph and plot space
-    CPTGraph *graph = self.hostView.hostedGraph;
-    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *) graph.defaultPlotSpace;
-    
-    // 2 - Create the three plots
-    CPTScatterPlot *aaplPlot = [[CPTScatterPlot alloc] init];
-    aaplPlot.dataSource = self;
-    //aaplPlot.identifier = CPDTickerSymbolAAPL;
-    CPTColor *aaplColor = [CPTColor redColor];
-    [graph addPlot:aaplPlot toPlotSpace:plotSpace];
-    
-    // 3 - Set up plot space
-    [plotSpace scaleToFitPlots:[NSArray arrayWithObjects:aaplPlot, nil]];
-    CPTMutablePlotRange *xRange = [plotSpace.xRange mutableCopy];
-    [xRange expandRangeByFactor:CPTDecimalFromCGFloat(1.1f)];
-    plotSpace.xRange = xRange;
-    CPTMutablePlotRange *yRange = [plotSpace.yRange mutableCopy];
-    [yRange expandRangeByFactor:CPTDecimalFromCGFloat(1.2f)];
-    plotSpace.yRange = yRange;
-    
-    // 4 - Create styles and symbols
-    CPTMutableLineStyle *aaplLineStyle = [aaplPlot.dataLineStyle mutableCopy];
-    aaplLineStyle.lineWidth = 2.5;
-    aaplLineStyle.lineColor = aaplColor;
-    aaplPlot.dataLineStyle = aaplLineStyle;
-    CPTMutableLineStyle *aaplSymbolLineStyle = [CPTMutableLineStyle lineStyle];
-    aaplSymbolLineStyle.lineColor = aaplColor;
-    CPTPlotSymbol *aaplSymbol = [CPTPlotSymbol ellipsePlotSymbol];
-    aaplSymbol.fill = [CPTFill fillWithColor:aaplColor];
-    aaplSymbol.lineStyle = aaplSymbolLineStyle;
-    aaplSymbol.size = CGSizeMake(6.0f, 6.0f);
-    aaplPlot.plotSymbol = aaplSymbol;
-}
-
--(void)configureAxes
-{
-    
 }
 
 @end
