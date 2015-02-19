1f5eb59620fa466b08401895eabb6fcedeb262b4
Harrison Ferrone <hferrone@gmail.com>
2015-02-19 13:14:35 -0600

    Revert

--- OpenGYM_MVP/MainMapViewController.m
+++ OpenGYM_MVP/MainMapViewController.m
@@ -77,6 +77,7 @@
                    annotation.coordinate = place.location.coordinate;
                    annotation.title = object[@"title"];
                    annotation.sport = object[@"sport"];
+                   
                    self.allEvents = [NSMutableArray arrayWithObject:annotation];
                    [self.mapView addAnnotations:self.allEvents];
                }
